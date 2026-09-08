1abd929f76376b52923274f2c07c3f92c58e696b
Kathia <kathiahonor@gmail.com>
2026-09-08 17:44:32 -0400

    tortura

---
+++ 9.Clase08092026/script/ejemplo1.js
@@ -0,0 +1,27 @@
+//lista de tareas 
+let tareas =[];
+function agregarTareas(tarea= "",estado="incompleta"){
+    if (tarea === ""){
+    tarea = document.getElementById("tarea"). value;
+tarea =document.getElementById("tareasSelect"). value;
+    }
+    tareas.push({descripcion: tarea, completada:estado});
+    }
+function eliminarTarea(indice){
+    if (indice >=0 && indice < tareas.length){
+        tareas.splice(indice, 1);
+    } else{
+        console.log("Índice inválido");
+    }
+}
+function mostrarTareas(){
+    console.log("Mostrando lista de tareas:");
+    tareas.forEach((tarea, indice) => {
+        console.log(`${indice + 1} . [${tarea.completada }] ${tarea. descripcion}`);
+    });
+}
+agregarTareas("Comprar leche","completada");
+agregarTareas("Hacer ejercicio","completa");
+agregarTareas("Estudiar JavaScript","incompleta");
+agregarTareas("Subir a GitHub","incompleta");
+mostrarTareas();
